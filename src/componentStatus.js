bb24d37d0444dd61c68bc957e27f1f1011a0844d
Shubham Raj Pandit <shubhamraj465@gmail.com>
2021-12-11 21:33:42 +0530

    Added Multiple Backgrounds

--- src/componentStatus.js
+++ src/componentStatus.js
@@ -8,5 +8,8 @@ const choicePages = ["room-3", "room-18"]
 const validateAnswer = ["room-4", "room-6", "room-7", "room-8", "room-9", "room-12", "room-13", "room-15", "room-17"]
 const noBottom = ["room-19", "end-1", "end-2", "end-3", "end-4", "end-5", "end-6", "time-end", "winner"]
 const repeatPages = ["room-4", "room-5", "room-6", "room-7", "room-8", "room-9", "room-10", "room-11", "room-12", "room-13", "room-14", "room-15", "room-16", "room-18", "room-19", "room-20"]
+const homeImages = ["home-page", "welcome", "instructions", "end-1", "end-2", "end-3", "end-4", "end-5", "end-6", "time-end", "winner"]
+const scenarioImages = ["scenario", "the-adventure-begins", "room-1", "room-2", "room-7", "room-8", "room-11", "room-13", "room-18", "room-19", "room-20"]
+const diversionImages = ["room-3", "room-4", "room-5", "room-6", "room-9", "room-10", "room-12", "room-14", "room-15", "room-16", "room-17"]
 
-export {rollPages, noInput, noHeader, backButton, startPages, choicePages, wrongSame, validateAnswer, noBottom, repeatPages}
+export {rollPages, noInput, noHeader, backButton, startPages, choicePages, wrongSame, validateAnswer, noBottom, repeatPages, homeImages, scenarioImages, diversionImages}
--- src/components/Background.js
+++ src/components/Background.js
@@ -1,20 +1,70 @@
-import landscape from '../images/landscape-image.jpg'
-import portrait from '../images/portrait-image.jpg'
+import {useState, useEffect} from 'react'
+import {homeImages, scenarioImages, diversionImages} from '../componentStatus'
+
+import home_landscape from '../images/landscape-image.jpg'
+import home_portrait from '../images/portrait-image.jpg'
+import scenario_landscape from '../images/scenario-landscape.jpg'
+import scenario_portrait from '../images/scenario-portrait.jpg'
+import diversion_landscape from '../images/diversion-landscape.jpg'
+import diversion_portrait from '../images/diversion-portrait.jpg'
 import audio from '../soundtracks/home_sound.mp3'
 
-const Background = ({page, soundState, changeBackground}) => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  let background = landscape
-
-  if (width > height){
-    background = landscape;
-    changeBackground(background)
+const checkBackground = () => {
+  if (window.innerWidth > window.innerHeight){
+    return home_landscape
   }
   else{
-    background = portrait;
-    changeBackground(background)
+    return home_portrait
   }
+}
+
+const Background = ({page, soundState, changeBackground}) => {
+  const [background, updateBackground] = useState(checkBackground())
+
+  useEffect(() => {
+    if (scenarioImages.includes(page)){
+      if (window.innerWidth > window.innerHeight){
+        setTimeout(() => {
+          updateBackground(scenario_landscape)
+          changeBackground(scenario_landscape)
+        }, 2500)
+      }
+      else{
+        setTimeout(() => {
+          updateBackground(scenario_portrait)
+          changeBackground(scenario_portrait)
+        }, 2500)
+      }
+    }
+    else if (diversionImages.includes(page)){
+      if (window.innerWidth > window.innerHeight){
+        setTimeout(() => {
+          updateBackground(diversion_landscape)
+          changeBackground(diversion_landscape)
+        }, 2500)
+      }
+      else{
+        setTimeout(() => {
+          updateBackground(diversion_portrait)
+          changeBackground(diversion_portrait)
+        }, 2500)
+      }
+    }
+    else if (homeImages.includes(page)){
+      if (window.innerWidth > window.innerHeight){
+        setTimeout(() => {
+          updateBackground(home_landscape)
+          changeBackground(home_landscape)
+        }, 2500)
+      }
+      else{
+        setTimeout(() => {
+          updateBackground(home_portrait)
+          changeBackground(home_portrait)
+        }, 2500)
+      }
+    }
+  }, [page])
 
   const tags = [
     <div
